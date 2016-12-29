3f7c4813886432c978914ecc18e16d06ddb246c0
pranithmididoddi <pranith13@gmail.com>
2016-12-28 20:07:15 -0600

    string/multithread/regex/collections basics

---
+++ src/Basics.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Pranith on 12/28/16.
+ */
+public class Basics {
+
+    public static void main(String[] args){
+        String s="Pranith mididoddi";
+
+        String sub=s.substring(0,2).toLowerCase();
+        System.out.println(sub);
+    }
+}
