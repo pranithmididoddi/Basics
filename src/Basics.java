fdc3628deec7265601f3c08dc4e7483cfa8e1724
pranithmididoddi <pranith13@gmail.com>
2016-12-30 15:45:30 -0600

    string/multithread/regex/math/collections basics

--- src/Basics.java
+++ src/Basics.java
@@ -1,12 +1,38 @@
 /**
  * Created by Pranith on 12/28/16.
  */
+
+/**Prime sum.*/
+import java.util.*;
 public class Basics {
 
     public static void main(String[] args){
-        String s="Pranith mididoddi";
+        int num=7;
 
-        String sub=s.substring(0,2).toLowerCase();
-        System.out.println(sub);
+        System.out.println(primesum(num));
+    }
+
+    public static ArrayList<Integer> primesum(int a){
+
+    ArrayList<Integer> myarray=new ArrayList<>();
+
+
+        for(int i=2;i<a;i++){
+        if(isPrime(i) && isPrime(a-i)){
+            myarray.add(i);
+            myarray.add(a-i);
+
+            return myarray;
+        }
+    }
+        return myarray;
+}
+
+    public static boolean isPrime(int n){
+        for(int i=2;i<n/2;i++){
+            if(n%i==0) return false;
+        }
+
+        return true;
     }
 }
