470c8e4b8948054503e5c454c3395b33a8ede2e9
pranithmididoddi <pranith13@gmail.com>
2016-12-30 15:51:28 -0600

    string/multithread/regex/math/collections basics

--- src/Basics.java
+++ src/Basics.java
@@ -35,4 +35,21 @@ public class Basics {
 
         return true;
     }
+/**Palindrome check*/
+    public boolean isPalindrome(int a) {
+
+        int temp=a;
+        int remainder=0;
+        int sum=0;
+
+        while(temp>0){
+            remainder=temp%10;
+            sum=sum*10+remainder;
+            temp=temp/10;
+        }
+
+        if(sum==a) return true;
+
+        return false;
+    }
 }
