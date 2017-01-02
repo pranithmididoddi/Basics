f29d054ce713b37852342eb08dae8756e0258733
pranithmididoddi <pranith13@gmail.com>
2017-01-01 19:04:15 -0600

    string/multithread/regex/math/collections basics

--- src/Basics.java
+++ src/Basics.java
@@ -14,26 +14,51 @@ public class Basics{
     }
 
     public static String largestNumber(int[] nums){
-        String[] arr=new String[nums.length];
+        String[] res=new String[nums.length];
+        StringBuffer str=new StringBuffer();
+
         for(int i=0;i<nums.length;i++){
-            arr[i]=String.valueOf(nums[i]);
+            res[i]=String.valueOf(nums[i]);
         }
-        Arrays.sort(arr, new Comparator<String>() {
+
+        Arrays.sort(res, new Comparator<String>() {
             @Override
             public int compare(String o1, String o2) {
                 return (o2+o1).compareTo(o1+o2);
             }
         });
 
-        StringBuilder sb=new StringBuilder();
-        for (String s : arr){
-            sb.append(s);
+        for(String o : res){
+         str.append(o);
         }
 
+        while(str.charAt(0)=='0'&&str.length()>1)
+        str.deleteCharAt(0);
 
-        while(sb.charAt(0)=='0'&&sb.length()>1)
-            sb.deleteCharAt(0);
-
-        return sb.toString();
+    return str.toString();
     }
+
+//    public static String largestNumber(int[] nums){
+//        String[] arr=new String[nums.length];
+//        for(int i=0;i<nums.length;i++){
+//            arr[i]=String.valueOf(nums[i]);
+//        }
+//        Arrays.sort(arr, new Comparator<String>() {
+//            @Override
+//            public int compare(String o1, String o2) {
+//                return (o2+o1).compareTo(o1+o2);
+//            }
+//        });
+//
+//        StringBuilder sb=new StringBuilder();
+//        for (String s : arr){
+//            sb.append(s);
+//        }
+//
+//
+//        while(sb.charAt(0)=='0'&&sb.length()>1)
+//            sb.deleteCharAt(0);
+//
+//        return sb.toString();
+//    }
 }
